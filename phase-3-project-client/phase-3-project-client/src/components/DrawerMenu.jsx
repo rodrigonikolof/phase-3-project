3f53ae7b6af14f0fe7f4a3fb639ca89222da1b7a
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-02-18 13:55:26 +1100

    Added temporary drawer

---
+++ phase-3-project-client/phase-3-project-client/src/components/DrawerMenu.jsx
@@ -0,0 +1,20 @@
+import { styled } from '@mui/system';
+import { Typography, Drawer, ListItem, List, ListItemIcon, ListItemText, ListItemButton, AppBar, Toolbar, Box, IconButton, Button, Container } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import AddIcon from '@mui/icons-material/Add';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
+import TextSnippetIcon from '@mui/icons-material/TextSnippet';
+import { useNavigate } from "react-router-dom";
+
+export default function DrawerMenu(){
+    {console.log('hello')}
+return(
+    <>
+        
+
+
+    </>
+)
+
+}
--- phase-3-project-client/phase-3-project-client/src/components/Layout.jsx
+++ phase-3-project-client/phase-3-project-client/src/components/Layout.jsx
@@ -1,35 +1,39 @@
 import { styled } from '@mui/system';
-import { Typography, Drawer, ListItem, List, ListItemIcon, ListItemText, ListItemButton } from '@mui/material';
+import { Typography, Drawer, ListItem, List, ListItemIcon, ListItemText, ListItemButton, AppBar, Toolbar, Box, IconButton, Button, Container } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import AddIcon from '@mui/icons-material/Add';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
 import TextSnippetIcon from '@mui/icons-material/TextSnippet';
 import { useNavigate } from "react-router-dom";
+import DrawerMenu from './DrawerMenu'
 
 
-const drawerWidth = 220
+// const drawerWidth = 220
 
-const useStyles = makeStyles({
-  page: {
-    background: '#f9f9f9',
-    width: '100%',
-  },
-  root: {
-    display: 'flex',
-  },
-  drawer: {
-    width: drawerWidth,
+// const useStyles = makeStyles({
+//   page: {
+//     background: '#f9f9f9',
+//     width: '100%',
+//   },
+//   root: {
+//     display: 'flex',
+//   },
+//   drawer: {
+//     width: drawerWidth,
     
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-})
+//   },
+//   drawerPaper: {
+//     width: drawerWidth,
+//   },
+// })
 
 
 
 export default function Layout({children}){
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false)
     const navigate = useNavigate()
-    const classes = useStyles()
+    // const classes = useStyles()
 
     const menuItems = [
         { 
@@ -50,39 +54,97 @@ export default function Layout({children}){
     })
 
     return (
-        <div className={classes.root}>
-             <Drawer
-                className={classes.drawer}
-                variant="permanent"
-                classes={{ paper: classes.drawerPaper }}
-                anchor="left"
-                > 
-                <div>
-                    <Typography variant="h5" className={classes.title}> Note Tracker </Typography>
-                </div>
+<div>
+<Box sx={{ flexGrow: 1, mb: 3  }}>
+      <AppBar position="static" color='secondary'>
+        <Toolbar>
+          <IconButton
+            onClick={()=>setIsDrawerOpen(true)}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>  
 
-                    <List>
-                    {menuItems.map((item) => (
-                        <ListItem 
-                        key={item.text} 
-                        >
-                            <ListItemButton 
-                                onClick={()=>navigate(item.path)} 
-                            >
-                        <ListItemIcon>{item.icon}</ListItemIcon>
-                        <ListItemText primary={item.text} />
-                        </ListItemButton>
-                        </ListItem>
-                    ))}
-                    </List>
-            </Drawer>
+    <Drawer
+    anchor='left'
+    open={isDrawerOpen}
+    onClose={()=>setIsDrawerOpen(false)}
+    >
+      <Box>
+      <Container sx={{mt:2}}>
+        <Typography variant="h5"> Note Tracker </Typography>
+      </Container>
+          <List>
+          {menuItems.map((item) => (
+              <ListItem 
+              key={item.text} 
+              >
+                <ListItemButton 
+                    onClick={()=>{
+                      navigate(item.path)
+                      setIsDrawerOpen(false)
+                    }} 
+                >
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ListItem>
+          ))}
+          </List>        
+      </Box>
+    </Drawer>
+
+  
+  <div className="page-container">
+      {children}
+  </div>
+</div>
+
+        // <div className={classes.root}>
+
+        //      <Drawer
+        //         className={classes.drawer}
+        //         variant="permanent"
+        //         classes={{ paper: classes.drawerPaper }}
+        //         anchor="left"
+        //         > 
+                // <div>
+                //     <Typography variant="h5" className={classes.title}> Note Tracker </Typography>
+                // </div>
+
+                //     <List>
+                //     {menuItems.map((item) => (
+                //         <ListItem 
+                //         key={item.text} 
+                //         >
+                //             <ListItemButton 
+                //                 onClick={()=>navigate(item.path)} 
+                //             >
+                //         <ListItemIcon>{item.icon}</ListItemIcon>
+                //         <ListItemText primary={item.text} />
+                //         </ListItemButton>
+                //         </ListItem>
+                //     ))}
+                //     </List>
+        //     </Drawer>
             
 
-            <Styled>
-            <div className="page-container">
-                {children}
-            </div>
-            </Styled>
-        </div>
+        //     <Styled>
+        //     <div className="page-container">
+        //         {children}
+        //     </div>
+        //     </Styled>
+        // </div>
     )
 }
