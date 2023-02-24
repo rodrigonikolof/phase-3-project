b951b4e7009396f110d3a27dbcf837f17388cec5
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-02-24 13:46:40 +1100

    created Sinatra routes for each request

--- phase-3-project-server/app/controllers/application_controller.rb
+++ phase-3-project-server/app/controllers/application_controller.rb
@@ -1,14 +1,35 @@
 class ApplicationController < Sinatra::Base
-  # set :default_content_type, 'application/json'
+  set :default_content_type, 'application/json'
   
-  # Add routes
+    get '/users' do
+      User.all.to_json
+    end
 
-  get '/' do
-    "HELLO WORLD!!!"
-  end
+    get '/notes/user/:id' do 
+      Note.where(user_id: params[:id]).to_json
+    end
 
-  get '/users' do
-    User.all.to_json
-  end
+    delete '/notes/:id' do
+      Note.find(params[:id]).destroy
+    end
+
+    post '/notes' do
+     note = Note.create(
+      title: params[:title],
+      details: params[:details],
+      category: params[:category],
+      user_id: params[:user]
+     )
+    end
+
+    patch '/notes/:id' do
+      note = Note.find(params[:id])
+      note.update(
+        title: params[:title],
+      details: params[:details],
+      category: params[:category]
+      )
+      note.to_json
+    end
 
 end
